37c847336e3ba771796e15c012088c540144abfe
Enumerable entity <alfred.dc.alpha@gmail.com>
2022-02-04 18:02:38 +0100

    Pojo created for incoming registrations requests

---
+++ src/main/java/com/financy/financyapp/controllers/AuthController.java
@@ -0,0 +1,46 @@
+package com.financy.financyapp.controllers;
+
+import com.financy.financyapp.models.pojo.UserRegistrationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(path = "/auth")
+public class AuthController {
+
+    //UserService userService;
+
+//    @Autowired
+//    public AuthController (UserService userService){
+//        this.userService=userService;
+//    }
+
+    @GetMapping("/login")
+    String getLoginPage(){
+        return "login";
+    }
+
+    @GetMapping(path = "/register")
+    ModelAndView getRegtistrationPage(ModelAndView modelAndView){
+        modelAndView.addObject("userData", new UserRegistrationRequest());
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+    @PostMapping(path = "/register")
+    String registerNewUser(@Valid @ModelAttribute UserRegistrationRequest user, HttpServletRequest httpServletRequest, BindingResult bindingResult){
+
+       // userService.register(user, httpServletRequest);
+
+        return "redirect:/";
+    }
+
+}
