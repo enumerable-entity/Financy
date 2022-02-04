4e0b13ee619742642655b46982c24a4696872f1b
Enumerable entity <alfred.dc.alpha@gmail.com>
2022-02-04 19:40:31 +0100

    User authentication implementation done

---
+++ src/main/java/com/financy/financyapp/emums/Permission.java
@@ -0,0 +1,14 @@
+package com.financy.financyapp.emums;
+
+public enum Permission {
+    READ("read"), WRITE("write");
+
+    private final String permission;
+
+    Permission(String permission) {
+        this.permission = permission;
+    }
+    public String getPermission() {
+        return permission;
+    }
+}
